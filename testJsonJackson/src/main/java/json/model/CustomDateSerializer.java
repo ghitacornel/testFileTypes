c8fd2583a142af23ee1c4ede9f484d004b965135
cornel <ghita.cornel@gmail.com>
2022-09-13 11:23:40 +0300

    better

--- testJsonJackson/src/main/java/json/model/CustomDateSerializer.java
+++ testJsonJackson/src/main/java/json/model/CustomDateSerializer.java
@@ -1,7 +1,6 @@
 package json.model;
 
 import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 
@@ -14,11 +13,7 @@ public class CustomDateSerializer extends StdSerializer<Date> {
     private final SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy hh:mm:ss");
 
     public CustomDateSerializer() {
-        this(null);
-    }
-
-    public CustomDateSerializer(Class t) {
-        super(t);
+        super(Date.class);
     }
 
     @Override
